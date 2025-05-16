a0277f61abf9615c51168aca3be17cf8169df807
RanUser01 <wilczek.janusz01@gmail.com>
2025-05-16 18:35:03 +0200

    blah balg

--- run_TCNP.sh
+++ run_TCNP.sh
@@ -5,12 +5,14 @@
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=300MB]"
-#BSUB -W 2:00
-#BSUB -o batch_output/%J.out
-#BSUB -e batch_output/%J.err
+#BSUB -W 1:30
+#BSUB -o TCNP_%J.out
+#BSUB -e TCNP_%J.err
 
 module load python3/3.10.13
+ 
+source venv/bin/activate
 
-source venv_1/bin/activate
+export HF_HOME="/work3/s243891"
 
-python TCNP.py checkpoint-.....
+python TCNP.py /work3/s243891/chest_xray_diffusion_overfit_800e/checkpoint-145000 
